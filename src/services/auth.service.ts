35a38c1aed3a3e8a5f6dae101916c2f458e67bb3
MARGAM VINAY <margamvinay77@gmail.com>
2025-07-24 15:16:01 +0530

    fix: error resolve

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -26,14 +26,14 @@ const setAuthCookies = (res: Response, accessToken: string, refreshToken: string
   const isProduction = process.env.NODE_ENV === 'production'
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
-    secure: isProduction,
+    secure: true,
     sameSite: 'none',
     maxAge: 15 * 60 * 1000, // 15 minutes
   })
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'strict' : 'lax',
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   })
 }
